a091675e0c93fb2fcd34b6811e197ad16a95ae1c
Aaron Winter <winter.aaron93@gmail.com>
2022-12-25 12:05:15 +0100

    make zecs a library

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
-const ecs = @import("entities.zig");
+const testing = std.testing;
+
+pub const Entities = @import("entities.zig").Enties;
+pub const Entity = @import("entities.zig").Entity;
+pub const World = @import("world.zig").World;
 
 const Position = struct {
     x: i32 = 0,
@@ -11,30 +15,38 @@ const Velocity = struct {
     y: i32 = 0,
 };
 
-var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-const alloc = gpa.allocator();
-
-pub fn main() anyerror!void {
-    var world = ecs.World.init(alloc);
+test "world" {
+    var world = try World.init(std.testing.allocator);
     defer world.deinit();
 
-    try setup(&world);
+    const e1 = try world.entities.spawn();
+    try world.entities.set(Position, e1, .{ .x = 0, .y = 5 });
+    try world.entities.set(Velocity, e1, .{ .x = 5, .y = 5 });
+
+    const e2 = try world.entities.spawn();
+    try world.entities.set(Position, e2, .{ .x = 4, .y = 5 });
+    try world.entities.set(Velocity, e2, .{ .x = 7, .y = 4 });
+
+    const e3 = try world.entities.spawn();
+    try world.entities.set(Position, e3, .{ .x = 5, .y = 5 });
+
+    try world.addSystem(testSystem);
+
+    var index: u32 = 0;
+    while (index < 10) : (index += 1) {
+        try world.run();
+    }
 }
 
-fn setup(world: *ecs.World) !void {
-    const entity1 = world.spawn();
-    try world.add(entity1, Position{ .x = 0, .y = 0 });
-    try world.add(entity1, Velocity{ .x = 5, .y = 0 });
+fn testSystem(world: *World) !void {
+    var query = try world.query(.{ .pos = Position, .vel = Velocity });
 
-    const entity2 = world.spawn();
-    try world.add(entity2, Position{ .x = 0, .y = 0 });
-    try world.add(entity2, Velocity{ .x = -5, .y = 0 });
+    var it = query.iter();
+    while (it.next()) |e| {
+        e.pos.x += e.vel.x;
+        e.pos.y += e.vel.y;
+        std.debug.print("Changed Position: ({},{})\n", .{ e.pos.x, e.pos.y });
+    }
 
-    const entity3 = world.spawn();
-    try world.add(entity3, Position{ .x = 0, .y = 0 });
-    try world.add(entity3, Velocity{ .x = 0, .y = 5 });
-
-    const entity4 = world.spawn();
-    try world.add(entity4, Position{ .x = 0, .y = 0 });
-    try world.add(entity4, Velocity{ .x = 0, .y = -5 });
+    std.debug.print("hello from testSystem\n", .{});
 }
